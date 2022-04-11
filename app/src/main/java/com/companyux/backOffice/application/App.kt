7fd457cdf0e0dd9ecc2d2de055b9e5a3e0d30e1f
JaWadEsL <javades755@gmail.com>
2022-04-11 14:49:19 +0430

    - set fontFamily on project

--- app/src/main/java/com/companyux/backOffice/application/App.kt
+++ app/src/main/java/com/companyux/backOffice/application/App.kt
@@ -1,10 +1,19 @@
 package com.companyux.backOffice.application
 
+import android.R
 import androidx.multidex.MultiDexApplication
+
+
 
 class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+//        CalligraphyConfig.initDefault(
+//            CalligraphyConfig.Builder()
+//                .setDefaultFontPath("fonts/Roboto-RobotoRegular.ttf")
+//                .setFontAttrId(R.attr.fontPath)
+//                .build()
+//        )
     }
 }
